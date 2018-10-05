cce216892929f128f0903fb5491c1805ef797353
Andrew Tan <andrewvmail@gmail.com>
2018-10-05 01:14:18 -0700

    working test runner wallaby for developing main

---
+++ main/wallaby.js
@@ -0,0 +1,24 @@
+module.exports = function (wallaby) {
+  return {
+    files: [
+      'app/**/*.js',
+      '!app/**/*.spec.js'
+    ],
+    tests: [
+      'app/**/*.spec.js',
+      'test/**/*.js'
+    ],
+    env: {
+      type: 'node',
+      runner: 'node'
+    },
+    compilers: {
+      '**/*.js': wallaby.compilers.babel({
+        // Tell Wallaby to use Ava's Babel preset, necessary if your project doesn't use Babel otherwise.
+        presets: ['@ava/babel-preset-stage-4']
+      })
+    },
+    testFramework: 'ava',
+    debug: true
+  };
+};
