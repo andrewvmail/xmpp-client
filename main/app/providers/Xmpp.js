d51eef712b5e414b018dd9ce4417e5ea89cdf6c0
Andrew Tan <andrewvmail@gmail.com>
2018-10-09 23:49:28 -0700

    refactor configuration binding

--- main/app/providers/Xmpp.js
+++ main/app/providers/Xmpp.js
@@ -2,43 +2,42 @@ import XMPP from "stanza.io";
 
 const Xmpp = ({ onConnected, onSessionBound, onPresence, config }) => {
   let client;
-  let test;
 
   return context => {
     return {
       createClient({ username, password }) {
-        test = 1
+        const { domain } = config;
+
         client = XMPP.createClient({
-          jid: username + "@" + config.domain,
+          jid: username + "@" + domain,
           password,
           transport: "websocket",
-          wsURL: "ws://localhost:5280/ws-xmpp"
+          wsURL: "ws://" + domain + ":5280/ws-xmpp"
         });
 
         client.on("session:bound", () => {
           context.app.runSequence("xmppSessionBound", onSessionBound);
         });
 
-        client.on("presence", (payload) => {
-          const {nick} = payload
-          context.app.runSequence("onPresence", onPresence, {nick});
+        client.on("presence", payload => {
+          const { nick } = payload;
+          context.app.runSequence("onPresence", onPresence, { nick });
 
-          if(payload.from.local === payload.to.local) {
+          if (payload.from.local === payload.to.local) {
             // we want to bootstrap here later
             // context.app.runSequence("messagingBootstrap", [])
-            return
+            return;
           }
         });
       },
       connect() {
-        client.connect()
+        client.connect();
       },
-      sendPresence({nick}) {
-        client.sendPresence({nick})
+      sendPresence({ nick }) {
+        client.sendPresence({ nick });
       }
     };
   };
-}
-
+};
 
 export default Xmpp;
